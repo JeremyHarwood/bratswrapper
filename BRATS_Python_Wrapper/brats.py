2fc267693b9765183d0f787569746d13ba52150c
JeremyHarwood <jeremy.harwood@physics.org>
2020-07-08 18:55:30 +0100

    Multiprocessing added (multiexec)

--- BRATS_Python_Wrapper/brats.py
+++ BRATS_Python_Wrapper/brats.py
@@ -11,31 +11,70 @@ Notes:
 """
 
 #pragma once
+import sys
+import os.path
 import subprocess
+import multiprocessing as mult 
 
-class bind:
+class Bind:
     """description of class"""
 
     def __init__(self, installdir: str):
-        self.__install = installdir
+        self._install = installdir
 
     def execfile(self, fileloc: str) -> int:
+        return _Script._execfile(self, fileloc)
+
+    def multiexec(self, filelist: list):
+        return _Multiexec._multiexec(self, filelist)
+
+
+class _Script(Bind):
+
+    def __init__(self): 
+        super(_Script, self).__init__()
+
+    def _execfile(self, fileloc: str) -> int:
 
         try:
             commandsfile = open(fileloc, "r")
         except FileNotFoundError:
             print("\n*** Error: File not found. Ensure the commands file %s exists ***\n" % (fileloc))
-            return 404
+            return {fileloc: 404}
         except Exception as e:
             print("\n*** Error: %s ***\n*** Unable to open the commands file %s for reading. Check the file exists and the appropriate permissions are set ***\n" % (str(e), fileloc))
-            return 404
+            return {fileloc: 401}
 
         try:
-            __process = subprocess.Popen([self.__install], stdin=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True)
+            __process = subprocess.Popen([self._install], stdin=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True)
             data = commandsfile.read()
             __process.communicate(input=data.strip())
             commandsfile.close()
-            return 0
+            return {fileloc: 0}
         except Exception as e:
             print("\n*** Error: %s ***\n*** BRATS processing failed***\n" % (e))
-            return 100
+            return {self._install: 404}
+
+
+class _Multiexec(_Script):
+
+    def __init__(self): 
+        super(Multiexec, self).__init__()
+
+    def _multiexec(self, filelist: list):
+
+        if os.path.exists(self._install) == False:
+            print("\n*** Error: File not found. Ensure the brats installation %s exists ***\n" % (self._install))
+            return {"Error": 404}
+
+        file_list = ["./testcommands.txt", "./testcommands_2.txt"] # Something we would probably create by reading  a directory
+
+        try: 
+            pool = mult.Pool(processes=2) # Run on 2 cores
+            returnvalue = pool.map(self.execfile, file_list)
+            pool.close()
+            pool.join()
+            return returnvalue
+        except Exception as e:
+            print("\n*** Error: %s ***\n*** BRATS processing failed***\n" % (e))
+            return {"Error": str(e)}
--- BRATS_Python_Wrapper/bratswapper_usageexample.py
+++ BRATS_Python_Wrapper/bratswapper_usageexample.py
@@ -2,7 +2,18 @@
 # -*- coding: utf-8 -*-
 
 import brats
+import multiprocessing as mult # Only needed for the MP example
 
-bratsopen = brats.bind("/soft/brats/brats")
 
-bratsopen.execfile("testcommands.txt")
+# Bind the BRATS install location
+test = brats.Bind("/soft/brats/brats") 
+
+# As we are now bound to the class we can run multiple files and/or methods either in series with execfile()...
+retvalsingle = test.execfile("testcommands.txt") # This is an instance
+#test.execfile("testcommands_2.txt") # This is another instance
+print(retvalsingle)
+
+# Or in parallel with multiexec...
+files = ["./testcommands.txt", "./testcommands_2.txt"] # List of files e.g. read from a directory
+retvalmulti = test.multiexec(files,) # Syntax: multiexec(list ["files", "to", "process"], int number_of_cores)
+print(retvalmulti)
