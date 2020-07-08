55fd298f88bcceb085f6bd83731eccf2194bd6e8
JeremyHarwood <jeremy.harwood@physics.org>
2020-07-08 20:02:09 +0100

    Added Documentation. Refactored.

--- BRATS_Python_Wrapper/brats.py
+++ BRATS_Python_Wrapper/brats.py
@@ -1,13 +1,33 @@
 """
+######### Python wrapper for the Broadband Radio Astronomy Tools (BRATS) ########
 
-Notes:
-    
-    There should only be one polarization intent per observation i.e one leakege cal, one angle cal. If this is not the case, it should be split off to make it so before the pipeline is run.
-    Polarisation calibration currently setup for L, S, and C band
-    Stokes I currently setup for P, L, S, C, and X band
+This is the first iteration of the Python wrapper for BRATS.
+
+This early version currently only supports the running of command files in which 
+BRATS commands, tasks, and parameters should be placed in the order by which they
+are to be processed, with one command per line.
+
+Single core and multiprocessing are both supported.
+
+See bratswrapper_usageexample.py and accompanying .txt files for example usage.
+
+***************************** IMPORTANT PLEASE READ  *****************************
+
+ Note there is no BRATS command and syntax error checking in this early version!
+ Only Python errors are handled. Incorrect commands are ignored which may lead to
+ unexpected results or failure to complete the run, particularly when setting
+ paramaters within a task.
+ It is therefore suggested that a manual test run is performed on a sample dataset
+ by simply running BRATS, then copying and pasting the contents of the file into
+ the terminal.
+
+ *********************************************************************************
+
 
 @author: Jeremy Harwood
 @email: jeremy.harwood@physics.org
+
+##################################################################################
 """
 
 #pragma once
@@ -16,61 +36,59 @@ import os.path
 import subprocess
 import multiprocessing as mult 
 
+# Base class for the BRATS wrapper
 class Bind:
-    """description of class"""
 
-    def __init__(self, installdir: str):
-        self._install = installdir
+    def __init__(self, install_dir: str):
+        self._install = install_dir
 
-    def execfile(self, fileloc: str) -> int:
-        return _Script._execfile(self, fileloc)
+    def execfile(self, file_loc: str) -> dict:
+        return _Script._execfile(self, file_loc)
 
-    def multiexec(self, filelist: list):
-        return _Multiexec._multiexec(self, filelist)
+    def multiexec(self, file_list: list, num_proc: int) -> dict:
+        return _Multiexec._multiexec(self, file_list, num_proc)
 
-
+# Child class: processes command files and executes 
 class _Script(Bind):
-
+     
     def __init__(self): 
         super(_Script, self).__init__()
 
-    def _execfile(self, fileloc: str) -> int:
+    def _execfile(self, file_loc: str) -> dict:
 
         try:
-            commandsfile = open(fileloc, "r")
+            commandsfile = open(file_loc, "r")
         except FileNotFoundError:
-            print("\n*** Error: File not found. Ensure the commands file %s exists ***\n" % (fileloc))
-            return {fileloc: 404}
+            print("\n*** Error: File not found. Ensure the commands file %s exists ***\n" % (file_loc))
+            return {file_loc: 404}
         except Exception as e:
-            print("\n*** Error: %s ***\n*** Unable to open the commands file %s for reading. Check the file exists and the appropriate permissions are set ***\n" % (str(e), fileloc))
-            return {fileloc: 401}
+            print("\n*** Error: %s ***\n*** Unable to open the commands file %s for reading. Check the file exists and the appropriate permissions are set ***\n" % (str(e), file_loc))
+            return {file_loc: 401}
 
         try:
             __process = subprocess.Popen([self._install], stdin=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True)
             data = commandsfile.read()
             __process.communicate(input=data.strip())
             commandsfile.close()
-            return {fileloc: 0}
+            return {file_loc: 0}
         except Exception as e:
             print("\n*** Error: %s ***\n*** BRATS processing failed***\n" % (e))
             return {self._install: 404}
 
-
+# Child class: handles multiprocessing for command files
 class _Multiexec(_Script):
+    
 
     def __init__(self): 
         super(Multiexec, self).__init__()
 
-    def _multiexec(self, filelist: list):
+    def _multiexec(self, file_list: list, num_proc: int) -> dict:
 
         if os.path.exists(self._install) == False:
             print("\n*** Error: File not found. Ensure the brats installation %s exists ***\n" % (self._install))
             return {"Error": 404}
-
-        file_list = ["./testcommands.txt", "./testcommands_2.txt"] # Something we would probably create by reading  a directory
-
         try: 
-            pool = mult.Pool(processes=2) # Run on 2 cores
+            pool = mult.Pool(num_proc) # Run on 2 cores
             returnvalue = pool.map(self.execfile, file_list)
             pool.close()
             pool.join()
--- BRATS_Python_Wrapper/bratswapper_usageexample.py
+++ BRATS_Python_Wrapper/bratswapper_usageexample.py
@@ -2,18 +2,17 @@
 # -*- coding: utf-8 -*-
 
 import brats
-import multiprocessing as mult # Only needed for the MP example
-
 
 # Bind the BRATS install location
-test = brats.Bind("/soft/brats/brats") 
+test = brats.Bind("/soft/brats/brats")
 
 # As we are now bound to the class we can run multiple files and/or methods either in series with execfile()...
-retvalsingle = test.execfile("testcommands.txt") # This is an instance
-#test.execfile("testcommands_2.txt") # This is another instance
-print(retvalsingle)
+return_val_1 = test.execfile("testcommands_1.txt") # This is an instance
+return_val_2 = test.execfile("testcommands_2.txt") # This is another instance
+print(return_val_1) # Returns a dictionary {file : returncode}. Returns 0 on success, else returns an error code.
+print(return_val_2)
 
 # Or in parallel with multiexec...
-files = ["./testcommands.txt", "./testcommands_2.txt"] # List of files e.g. read from a directory
-retvalmulti = test.multiexec(files,) # Syntax: multiexec(list ["files", "to", "process"], int number_of_cores)
-print(retvalmulti)
+files = ["./testcommands_1.txt", "./testcommands_2.txt"] # List of files e.g. read from a directory
+return_val_multi = test.multiexec(files,2) # Syntax: multiexec(list ["files", "to", "process"], int number_of_cores)
+print(return_val_multi) # Returns a dictionary {commandfilename : returncode}. Returns 0 on success, else returns an error code.
